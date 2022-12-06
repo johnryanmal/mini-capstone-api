c5800353c60d9a36a125d6e112555f75e6edda16
“johnryanmal” <johnryanmal@gmail.com>
2022-12-06 12:03:06 -0800

    write seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+100.times do
+  Product
+  .new(
+    name: Faker::Commerce.product_name,
+    description: 'No description.',
+    price: Faker::Commerce.price,
+    image_url: Faker::Internet.url(host: 'example.com', path: '/images/' + Faker::Internet.uuid)
+  )
+  .save
+end
