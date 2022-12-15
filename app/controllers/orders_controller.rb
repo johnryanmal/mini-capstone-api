23205114b86088f03b47dff991811d89f9af315b
“johnryanmal” <johnryanmal@gmail.com>
2022-12-15 10:27:11 -0800

    fix order create

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,12 +2,16 @@ class OrdersController < ApplicationController
   def index
     check_user do
       orders = Order.where(user_id: current_user.id)
-      render json: {orders: orders}
+      render json: orders.map { |order| order.deep_serialize }
     end
   end
 
   def create
-    product = Product.find_by(params.permit(:id))
+    product = Product.find_by(id: params[:product_id])
+    pp product
+    pp product.price
+    pp product.tax
+    pp product.total
 
     check_user do
       render json: Order.create(
@@ -30,7 +34,7 @@ class OrdersController < ApplicationController
     check_user do
       order = Order.find_by(params.permit(:id))
       if order&.user_id == current_user.id
-        render json: {order: order}
+        render json: order.deep_serialize
       else
         render json: {msg: "You don't have access to that order"}, status: :unauthorized
       end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,28 @@
 class Order < ApplicationRecord
   belongs_to :product
+
+  def view
+    {
+      methods: []
+    }
+  end
+
+
+  def deep_view
+    {
+      methods: view[:methods] + [:product_f]
+    }
+  end
+
+  def serialize
+    as_json(view)
+  end
+
+  def deep_serialize
+    as_json(deep_view)
+  end
+
+  def product_f
+    product.serialize
+  end
 end
